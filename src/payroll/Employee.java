add9bed66100e215e28794afcd112333c1246968
manosolireap <manosolireap@gmail.com>
2025-03-25 10:13:59 +0530

    initial commit

--- src/payroll/Employee.java
+++ src/payroll/Employee.java
@@ -1,7 +1,16 @@
 package payroll;
 
-public class Employee {
+public abstract class Employee {
     private String firstName;
     private String lastName;
     private String empId;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String empId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.empId = empId;
+    }
 }
